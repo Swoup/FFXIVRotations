ba4277d83279787099f064191ca9be1aa00ae0d1
StrawberryMage <placeholder@email.com>
2025-01-27 21:41:47 +0100

    Update rotation infos

--- Rotations/RotationInfo.cs
+++ Rotations/RotationInfo.cs
@@ -2,7 +2,7 @@ namespace Rotations;
 
 internal class RotationInfo
 { 
-    internal const string USERNAME = $"Swoup";
+    internal const string USERNAME = $"Strawberry Mage";
 
     internal const string VERSION = "1.0.0.0";
 }
